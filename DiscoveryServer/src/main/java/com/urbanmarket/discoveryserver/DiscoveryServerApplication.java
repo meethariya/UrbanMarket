c9ba5655fa83a75328da29da988cce7fb6729137
meethariya <meetchampian@gmail.com>
2024-04-09 16:42:48 +0530

    Added discovery server

---
+++ DiscoveryServer/src/main/java/com/urbanmarket/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 09-Apr-2024
+ * meeth
+ */
+package com.urbanmarket.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
