7f3d52a0b0e5e9c4997f2edf6abde813de3388f4
meethariya <meetchampian@gmail.com>
2024-05-19 17:40:22 +0530

    basic angular setup

--- urbanmarket-ui/src/app/modules/home/home.module.ts
+++ urbanmarket-ui/src/app/modules/home/home.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, AboutComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
