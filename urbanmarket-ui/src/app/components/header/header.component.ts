ebe9f5c98332902e4d8d0cc9d23f045eededf6d2
meethariya <meetchampian@gmail.com>
2024-05-21 10:57:16 +0530

    added header menu

--- urbanmarket-ui/src/app/components/header/header.component.ts
+++ urbanmarket-ui/src/app/components/header/header.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  Renderer2,
+  ElementRef,
+  HostListener,
+  Inject,
+} from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
   ngOnInit(): void {
-    console.log('header');
+    // Handle window load event
+    this.updateBackgroundColor();
   }
-  constructor() {}
+
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  /**
+   * On scroll event
+   */
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.updateBackgroundColor();
+  }
+
+  /**
+   * Update the background color of the component
+   */
+  private updateBackgroundColor() {
+    const div = this.el.nativeElement.querySelector('.header');
+    const scrollPosition =
+      this.document.documentElement.scrollTop ||
+      this.document.body.scrollTop ||
+      0;
+    if (scrollPosition > 100) {
+      this.renderer.addClass(div, 'scrolled');
+      this.renderer.removeClass(div, 'not-scrolled');
+    } else {
+      this.renderer.addClass(div, 'not-scrolled');
+      this.renderer.removeClass(div, 'scrolled');
+    }
+  }
 }
