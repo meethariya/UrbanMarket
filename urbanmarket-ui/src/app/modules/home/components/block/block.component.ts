5d2e516552dc71eee52d31b59a6b4843ea80427e
meethariya <meetchampian@gmail.com>
2024-05-26 11:47:19 +0530

    blocks component

---
+++ urbanmarket-ui/src/app/modules/home/components/block/block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.scss'
+})
+export class BlockComponent {
+
+}
--- urbanmarket-ui/src/app/modules/home/home.module.ts
+++ urbanmarket-ui/src/app/modules/home/home.module.ts
@@ -6,12 +6,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { PartnersComponent } from './components/partners/partners.component';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { BlockComponent } from './components/block/block.component';
 
 /**
  * Module for home pages such as /home, /about, /contact, /auth, etc.
  */
 @NgModule({
-  declarations: [HomeComponent, AboutComponent, PartnersComponent],
+  declarations: [HomeComponent, AboutComponent, PartnersComponent, BlockComponent],
   imports: [CommonModule, HomeRoutingModule, NgbTooltipModule],
 })
 export class HomeModule {}
